0f7bd6ba7a87cf70a7e7d8a80d95c14704d9e3b1
Ulric Ye <yye90012@gmail.com>
2018-09-14 23:47:43 -0700

    Buggy modal, need to fixthe HTML structure

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+window.onload = function() {
+    let span = document.getElementsByClassName("close");
+    let webProj = document.getElementsByClassName("web-proj");
+    let thumbnail = document.getElementsByClassName("thumbnail");
+
+    for(let i=0; i<thumbnail.length; i++) {
+        thumbnail[i].onclick = function() {
+            showModal(this.childNodes[0].id);
+        }
+    }
+}
+
+window.onclick = function(event) {
+    let modal = document.getElementsByClassName("web-proj");
+
+}
+
+function closeModal(projNum) {
+    let proj = document.getElementsByClassName("web-proj");
+    proj[projNum].style.display = "none";
+}
+
 function toggleTab(section) {
     let navmodal = document.getElementsByClassName("nav-modal");
     let index;
@@ -21,5 +43,21 @@ function toggleTab(section) {
     navmodal[index].style.display = "block";
 }
 
+function showModal(proj) {
+    let wProj = document.getElementsByClassName("web-proj");
+    let mContent = document.getElementsByClassName("modal-content");
+    for(let i=0; i<mContent.length; i++) {
+        mContent[i].style.display = "block";
+    }
+    switch(proj) {
+        case "Clostat":
+        wProj[0].style.display = "block";
+        break;
+        case "Markit":
+        wProj[1].style.display = "block";
+        break;
+    }
+}
 
-// const navModals = document.getElementsByClassName("nav-modal");
+
+
