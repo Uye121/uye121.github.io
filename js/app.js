3d8507f9b56fe85b05b06f0575945558b67f7217
Ulric Ye <yye90012@gmail.com>
2018-09-15 23:59:31 -0700

    Added resume, improved UI

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
+window.onclick = function(event) {
+    if(event.target.className != "thumbnail" && event.target.tagName.toLowerCase() != "img") {
+        let proj = document.getElementsByClassName("proj");
+        for(let i=0; i<proj.length; i++) {
+            proj[i].style.display = "none";
+        }
+    }
+}
+
 function openModal(projNum) {
-    let proj = document.getElementsByClassName("web-proj");
+    let proj = document.getElementsByClassName("proj");
     proj[projNum].style.display = "block";
 }
 
 function closeModal(projNum) {
-    let proj = document.getElementsByClassName("web-proj");
+    let proj = document.getElementsByClassName("proj");
     proj[projNum].style.display = "none";
 }
 
 function toggleTab(section) {
     let navmodal = document.getElementsByClassName("nav-modal");
+    console.log(navmodal);
     let index;
     switch(section) {
         case "about":
@@ -18,31 +28,32 @@ function toggleTab(section) {
         case "projects":
         index = 1;
         break;
-        case "experience":
+        case "resume":
         index = 2;
-        break;
-        case "involvement":
-        index = 3;
         break;
     }
     for(let i=0; i<navmodal.length; i++) {
         if(i != index) navmodal[i].style.display = "none";
     }
+    console.log(navmodal[index]);
     navmodal[index].style.display = "block";
 }
 
 function showModal(proj) {
-    let wProj = document.getElementsByClassName("web-proj");
+    let project = document.getElementsByClassName("proj");
     let mContent = document.getElementsByClassName("modal-content");
     for(let i=0; i<mContent.length; i++) {
         mContent[i].style.display = "block";
     }
     switch(proj) {
         case "Clostat":
-        wProj[0].style.display = "block";
+        project[0].style.display = "block";
         break;
         case "Markit":
-        wProj[1].style.display = "block";
+        project[1].style.display = "block";
+        break;
+        case "AppName":
+        project[2].style.display = "block";
         break;
     }
 }
