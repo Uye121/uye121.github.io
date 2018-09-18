81c50814d1251e9b3e199415e634cb2c9ae1ad01
Ulric Ye <yye90012@gmail.com>
2018-09-17 23:24:13 -0700

    Fixing background outside modal bug

--- js/app.js
+++ js/app.js
@@ -1,22 +1,67 @@
 window.onclick = function(event) {
-    if(event.target.className != "thumbnail" && event.target.tagName.toLowerCase() != "img") {
-        let proj = document.getElementsByClassName("proj");
-        for(let i=0; i<proj.length; i++) {
-            proj[i].style.display = "none";
+    let proj = document.getElementsByClassName("proj");
+    let projBackground = document.getElementById("projBackground");
+    let navBar = document.getElementsByClassName('nav-modal')[1];
+    let navBarStyle = window.getComputedStyle(navBar);
+    for(let i=0; i<proj.length; i++) {
+        let pStyle = window.getComputedStyle(proj[i]);
+        if(pStyle.getPropertyValue('display') == "block") {
+            let height = proj[i].scrollHeight-parseInt(navBarStyle.getPropertyValue('padding-top'));
+            projBackground.setAttribute("style", `height: ${height}px`);
         }
+    }
+
+    projBackground.onclick = function() {
+        console.log("gone");
+    }
+}
+
+window.onload = function() {
+    let thumbnails = document.getElementsByClassName("thumbnail");
+    for(let i=0; i<thumbnails.length; i++) {
+        thumbnails[i].addEventListener('mouseover', function(event) {
+            this.style.opacity = 0.6;
+            this.style.cursor = "pointer";
+            event.stopPropagation();
+        });
+        thumbnails[i].addEventListener('mouseleave', function(event) {
+            this.style.opacity = 1;
+            this.style.cursor = "default";
+            event.stopPropagation();
+        });
     }
 }
 
 function openModal(projNum) {
+    console.log("open modal");
     let proj = document.getElementsByClassName("proj");
-    proj[projNum].style.display = "block";
+    let projBackground = document.getElementById("projBackground");
+    let compStyle = window.getComputedStyle(projBackground);
+    if(compStyle.getPropertyValue('opacity') == "1") {
+        proj[projNum].style.display = "block";
+        projBackground.style.opacity = 0.5;
+        scrollToTop(1);
+    }
+}
+
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
 }
 
 function closeModal(projNum) {
     let proj = document.getElementsByClassName("proj");
+    let projBackground = document.getElementById("projBackground");
     proj[projNum].style.display = "none";
+    projBackground.style.opacity = 1;
 }
 
+// Toggle between the nav bar pages
 function toggleTab(section) {
     let navmodal = document.getElementsByClassName("nav-modal");
     console.log(navmodal);
@@ -41,10 +86,8 @@ function toggleTab(section) {
 
 function showModal(proj) {
     let project = document.getElementsByClassName("proj");
-    let mContent = document.getElementsByClassName("modal-content");
-    for(let i=0; i<mContent.length; i++) {
-        mContent[i].style.display = "block";
-    }
+    let mContent = document.getElementById("modal-content");
+    mContent.style.display = "block";
     switch(proj) {
         case "Clostat":
         project[0].style.display = "block";
@@ -57,6 +100,3 @@ function showModal(proj) {
         break;
     }
 }
-
-
-
