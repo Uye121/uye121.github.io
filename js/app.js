cdc5c9fdce36bb25e150108b1f060ae4cb280c9d
Ulric Ye <yye90012@gmail.com>
2019-01-01 22:39:40 -0800

    Fixed SW

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+window.onload = function() {
+    console.log("window loaded");
+    if ('serviceWorker' in navigator) {
+        // Use the window load event to keep the page load performant
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('/sw.js');
+        });
+      }
+}
+
 window.onclick = function(event) {
     let proj = document.getElementsByClassName("proj");
     let projBackground = document.getElementById("projBackground");
