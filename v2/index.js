4c5f5d9b10ee0ba31ee3791760c02a8b0add0249
Ulric Ye <yye90012@gmail.com>
2019-06-29 22:38:02 -0700

    Experimenting with tabs instead of hamburger menu

--- v2/index.js
+++ v2/index.js
@@ -1,30 +1,53 @@
-const cells = document.getElementsByClassName("project-cell");
+const projects = document.getElementsByTagName("figure");
 const modals = document.getElementsByClassName("overlay");
+const menuButton = document.getElementsByClassName("burger-icon");
 let modalOn = false;
 
-window.onload = function() {
+window.onload = function(event) {
+  event.stopPropagation();
   console.log("window loaded");
-  for(let i=0; i<cells.length; i++) {
-    cells[i].onclick = function() {
+  for(let i=0; i<projects.length; i++) {
+    projects[i].onclick = function() {
       hideAllModals();
-      cells[i].firstElementChild.style.opacity = 0.65;
-      modals[i].style.display = "block";
-      modalOn = true;
+      openModal(i);
     }
   }
+
+  menuButton[0].addEventListener("click", () => {
+    let mainNav = document.getElementsByClassName("main-nav")[0];
+    let burgerIcon = document.getElementsByClassName("burger-icon")[0];
+    let navWrapper = document.getElementsByClassName("nav-wrapper")[0];
+		if(mainNav.classList.contains("mobile-hide")) {
+			mainNav.classList.remove("mobile-hide");
+      mainNav.classList.add("mobile-show");
+      burgerIcon.style.color = "black";
+      // mainNav.style.opacity = 1.0;
+		} else {
+			mainNav.classList.add("mobile-hide");
+      mainNav.classList.remove("mobile-show");
+      burgerIcon.style.color = "white";
+      // mainNav.style.opacity = 0.0;
+		}
+  });
 }
 
-window.onclick = function(event) {
-  console.log(event.target);
-  if(modalOn && (event.target.className != "overlay") || (event.target.parentElement.className != "overlay")) {
-    // hideAllModals();
-    console.log("it should have worked");
-  }
+// Broken; it activates right after modalOn becomes true
+// window.onclick = function(event) {
+//   console.log(event.target);
+//   if(modalOn && (event.target.className != "overlay") || (event.target.parentElement.className != "overlay")) {
+//     hideAllModals();
+//   }
+// }
+
+function openModal(index) {
+  projects[index].firstElementChild.style.opacity = 0.65;
+  modals[index].style.display = "block";
+  modalOn = true;
 }
 
 function hideAllModals() {
-  for(let i=0; i<cells.length; i++) {
+  for(let i=0; i<projects.length; i++) {
     modals[i].style.display = "none";
-    cells[i].firstElementChild.style.opacity = 1;
+    projects[i].firstElementChild.style.opacity = 1;
   }
 }
